0cfa3899d61d66bcef519f936abb8ba4aaf87c98
Marcella Justo <71738159+justo-marcella@users.noreply.github.com>
2021-02-19 09:15:16 -0300

    utilizacao do fragment

--- src/components/componentForm/FaleConosco.js
+++ src/components/componentForm/FaleConosco.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 export default class FaleConosco extends Component {
     render() {
         return (
-            <div>
-                <h1>Contato</h1>
-                <div>
+
+            <Fragment>
+                <img src='https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg'></img>
+                <form>
                     <div>
-                        <img src='https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg'></img>
+                        <label htmlFor='nome'>Nome:</label>
+                        <input type='text' name='nome'/>
                     </div>
                     <div>
-                        <form>
-                            <div>
-                                <label htmlFor='nome'>Nome:</label>
-                                <input type='text' name='nome'/>
-                            </div>
-                            <div>
-                                <label htmlFor='email'>Email:</label>
-                                <input type='text' name='email'/>
-                            </div>
-                            <div>
-                                <label htmlFor='mensagem'>Mensagem:</label>
-                                <textarea rows='10' cols='50' type='text'></textarea>
-                            </div>
-                            <div>
-                                <button>Enviar</button>
-                            </div>
-                        </form>
+                        <label htmlFor='email'>Email:</label>
+                            <input type='text' name='email'/>
                     </div>
-                </div>
-                
-            </div>
+                    <div>
+                        <label htmlFor='mensagem'>Mensagem:</label>
+                        <textarea rows='10' cols='50' type='text'></textarea>
+                    </div>
+                    <div>
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </Fragment>
         )
     }
 }
