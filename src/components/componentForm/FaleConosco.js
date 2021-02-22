e7ec92088b81e93c644cff47adbdba6c892a0543
Marcella Justo <71738159+justo-marcella@users.noreply.github.com>
2021-02-22 17:38:45 -0300

    tentativa malsucedida de grid

--- src/components/componentForm/FaleConosco.js
+++ src/components/componentForm/FaleConosco.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import "./estiloContato.css"
 
 export default class FaleConosco extends Component {
     render() {
@@ -6,23 +7,27 @@ export default class FaleConosco extends Component {
 
             <Fragment>
                 <h1>Contato</h1>
-                <form>
+                <section className="contato">
+                    <img className="imgContato" src="https://www.mhemann.com.br/wp-content/uploads/2020/02/home-blog-box-slide-1-background-1.jpg"/>
+                <form className="formContato">
                     <div>
-                        <label htmlFor='nome'>Nome:</label>
-                        <input type='text' name='nome'/>
+                        <label className="labelContato" htmlFor='nome'>Nome:</label>
+                        <input className="inputForm" type='text' name='nome'/>
                     </div>
                     <div>
-                        <label htmlFor='email'>Email:</label>
-                            <input type='text' name='email'/>
+                        <label className="labelContato" htmlFor='email'>Email:</label>
+                        <input className="inputForm" type='text' name='email'/>
                     </div>
                     <div>
-                        <label htmlFor='mensagem'>Mensagem:</label>
-                        <textarea rows='10' cols='50' type='text'></textarea>
+                        <label className="labelContato" htmlFor='mensagem'>Mensagem:</label>
+                        <textarea className="inputForm" rows='10' cols='50' type='text'></textarea>
                     </div>
                     <div>
                         <button>Enviar</button>
                     </div>
                 </form>
+            
+            </section>
             </Fragment>
         )
     }
